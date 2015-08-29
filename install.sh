1913934b9346a217abd819959507c5d512be89d8
George Hotz <geohot@gmail.com>
2015-08-29 23:57:56 +0000

    try this

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install python-dev libffi-dev build-essential
-sudo pip install angr
+#sudo apt-get install python-dev libffi-dev build-essential
+pip install angr
 git clone https://github.com/angr/angr-doc.git
 
