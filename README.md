6be5e831d8e7758ee0668c7ca55ee6a82100161a
George Hotz <geohot@gmail.com>
2015-08-30 00:06:20 +0000

    minor updates

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 It should be green
 
+Install time is currently around 10 minutes. Get the time down to ~3 minutes and I can add it to QIRA.
+
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
 cd angr-doc/examples
 python whitehat_crypto400.py > /tmp/test_out
+
+echo -e "\n\n\n*** OUTPUT ***"
+cat /tmp/test_out
+
+# 0 return code here is success
+echo -e "\n\n\n*** GREP ***"
 grep "nytEaTBU" /tmp/test_out
 
 
